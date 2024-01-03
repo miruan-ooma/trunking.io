b56fda986168f629d66edee415a18918464c9a2c
ali <ali.kholmatov@ooma.com>
2024-01-03 11:37:37 +0100

    scss to sass

--- trunking/src/app/app.component.ts
+++ trunking/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'trunking';
