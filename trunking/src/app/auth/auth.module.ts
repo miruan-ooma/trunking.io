6b3b89fea766f629ec7315447b9c3e3917a49896
ali <ali.kholmatov@ooma.com>
2024-01-04 13:57:08 +0100

    login form

--- trunking/src/app/auth/auth.module.ts
+++ trunking/src/app/auth/auth.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AuthComponent } from "./auth.component";
 import { AuthRoutingModule } from "./auth-routing.module";
-import { MatCommonModule } from "@angular/material/core";
 import { MatCardModule } from "@angular/material/card";
 import { LoginComponent } from "./login/login.component";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent],
@@ -18,7 +18,8 @@ import { MatInputModule } from "@angular/material/input";
     AuthRoutingModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class AuthModule {}
