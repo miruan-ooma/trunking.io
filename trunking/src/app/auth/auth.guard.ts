cc67989f1dbd88320c1fc72cd5a94afc1eab14c6
ali <ali.kholmatov@ooma.com>
2024-01-05 14:13:52 +0100

    init auth guard

--- trunking/src/app/auth/auth.guard.ts
+++ trunking/src/app/auth/auth.guard.ts
@@ -1,13 +1,14 @@
 import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
 import { UserSessionService } from "../shared/services/user-session.service";
-import { map, tap } from "rxjs";
+import { delay, map, tap } from "rxjs";
 import { views } from "../../core/views";
 
 export const authGuard: CanActivateFn = () => {
   const userSessionService = inject(UserSessionService);
   const router = inject(Router);
   return userSessionService.user$.pipe(
+    delay(500),
     map(user => !!user),
     tap(user => !user && router.navigateByUrl(views.LOGIN))
   );
