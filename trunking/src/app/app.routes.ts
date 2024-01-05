c4aa1059ef2669cb7f4e3bd92ce94409c00e3422
ali <ali.kholmatov@ooma.com>
2024-01-05 13:56:16 +0100

    init auth guard

--- trunking/src/app/app.routes.ts
+++ trunking/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from "@angular/router";
+import { authGuard } from "./auth/auth.guard";
 
 export const routes: Routes = [
   { path: "auth", loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule) },
   {
     path: "dashboard",
     loadComponent: () =>
-      import("./dashboard/dashboard.component").then(mod => mod.DashboardComponent)
-  }
+      import("./dashboard/dashboard.component").then(mod => mod.DashboardComponent),
+    canActivate: [authGuard]
+  },
+  { path: "", redirectTo: "dashboard", pathMatch: "full" }
 ];
--- trunking/src/core/views.ts
+++ trunking/src/core/views.ts
@@ -1,3 +1,4 @@
 export const views = {
-  DASHBOARD: "/dashboard"
+  DASHBOARD: "/dashboard",
+  LOGIN: "/auth"
 };
