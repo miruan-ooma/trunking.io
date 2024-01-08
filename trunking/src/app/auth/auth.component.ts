242c3fb64b80aeac3a714525045ecf93eb344a41
ali <ali.kholmatov@ooma.com>
2024-01-08 19:52:13 +0100

    main page - added angular variables

--- trunking/src/app/auth/auth.component.ts
+++ trunking/src/app/auth/auth.component.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, HostBinding } from "@angular/core";
 
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
   styleUrl: "./auth.component.scss"
 })
-export class AuthComponent {}
+export class AuthComponent {
+  @HostBinding("class") class = "container container_centered";
+}
